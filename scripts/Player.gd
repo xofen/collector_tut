f7db0b6792bad34b75d1659b86669bc95665ea4d
xofen <87589625+xofen@users.noreply.github.com>
2023-12-22 23:44:34 -0300

    fix X flip -- Y idle switch

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -11,12 +11,17 @@ var can_move : bool = true
 
 
 func _process(_delta : float) -> void:
+	if (Input.is_action_pressed("up")):
+		sprite_2d.animation = "idle_back"
+	elif (Input.is_action_pressed("down")):
+		sprite_2d.animation = "idle"
+	
+	
 	direction.x = -int(Input.is_action_just_pressed("left")) + int(Input.is_action_just_pressed("right"))
 	direction.y = -int(Input.is_action_just_pressed("up")) + int(Input.is_action_just_pressed("down"))
 	
-	
 	if (direction.x == 0):
-		return;
+		return
 	
 	var isRight = direction.x > 0
 	sprite_2d.flip_h = isRight
@@ -27,9 +32,6 @@ func _physics_process(_delta : float) -> void:
 		can_move = false
 		movement_tween.run(self, global_position + direction * grid_size)
 		movement_tween.tween.finished.connect(on_movement_tween_finished)
-	
-	
-	
 
 
 func on_movement_tween_finished() -> void:
